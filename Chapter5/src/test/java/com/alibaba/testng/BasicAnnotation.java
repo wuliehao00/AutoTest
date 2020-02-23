11a82b9c493ea5d580ba2c14ace6520e96314db5
wuliehao <wuliehao00@163.com>
2020-02-23 13:45:32 +0800

    20200223提交

--- Chapter5/src/test/java/com/alibaba/testng/BasicAnnotation.java
+++ Chapter5/src/test/java/com/alibaba/testng/BasicAnnotation.java
@@ -1,9 +1,16 @@
 package com.alibaba.testng;
 
+import org.testng.annotations.Test;
+
 public class BasicAnnotation {
 
-    public static void main(String[] args) {
-        System.out.println("hello world");
+    @Test(groups = "server")
+    public void test(){
+        System.out.println("服务端运行");
     }
 
+    @Test(groups = "client")
+    public void test2(){
+        System.out.println("客户端运行");
+    }
 }
